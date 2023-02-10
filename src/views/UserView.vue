a6ec749e15693271704c887bd9cd548ca7c03b30
sehee-bang <i0475833@naver.com>
2023-02-10 16:19:37 +0900

    up

---
+++ src/views/UserView.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="user_profile">
+    <strong>User name : {{  userInfo.id }} </strong>
+    <b>Write : {{ userInfo.created }} </b>
+    <b>Karma : {{ userInfo.karma }}</b>
+  </div>
+</template>
+
+<script>
+import { createDOMCompilerError } from '@vue/compiler-dom';
+
+export default {
+    computed: {
+        userInfo() {
+            return this.$store.state.user;
+        }
+    },
+    created() {
+        this.$store.dispatch('FETCH_USER', this.$route.params.id)
+    }
+} 
+</script>
+
+<style scoped>
+.user_profile {font-size: 24px}
+.user_profile > * {display: block;}
+.user_profile > span {margin-right:5px}
+.user_profile > strong {color:#2455c3; }
+</style>
